13371543ea38779254e88b5d59ac5361b14537a4
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2022-08-31 11:33:35 +0530

    Create Saddlepoint.cpp

---
+++ Data Structures Lab/Lab Cycle 2/Saddlepoint.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int saddle_point(int** A,int m,int n)
+{
+    for(int i=0;i<m;i++)
+    {
+        int min=A[i][0];
+        int column_index=0;
+        for(int j=0;j<n;j++)
+        {
+            if(min>A[i][j])
+            {
+                min=A[i][j];
+                column_index=j;
+            }
+        }
+        int k = 0;
+        while(k<m && A[k][column_index] <= min)
+        {
+            k++;
+        }
+        if(k==m-1)
+        {
+            return A[k][column_index] ; 
+        }
+    }
+    return (-1);
+}
+int main()
+{
+    
+    int r,c;
+    int **A;
+    A = new int *[r];
+    cout<<"Enter the no: of rows : "<<" ";
+    cin>>r;
+    cout<<"Enter the no: of columns : "<<" ";
+    cin>>c;
+    for (int i = 0;i < r;i++) 
+    {
+		A[i] = new int[c];
+	}
+    cout<<"Input the elements of the matrix : "<<endl;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cin>>A[i][j];
+        }
+    }
+    cout<<"MATRIX = "<<endl;
+    cout<<"\t\t"<<endl;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cout<<setw(4)<<A[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    int s=saddle_point(A,r,c);
+    if (s==-1)
+    {
+        cout<<"No saddle point"<<endl;
+    }
+    else
+    {
+        cout<<"\nSaddle point of the matrix = "<<s<<endl;
+    }
+    return(0);
+}
